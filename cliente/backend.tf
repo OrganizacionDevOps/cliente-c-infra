3e28ba395342efe6fc0a445ef2739409287bc8db
servicesprueba <itoperations@optimizapp.com>
2025-07-07 16:24:51 -0500

    Update backend.tf

--- cliente/backend.tf
+++ cliente/backend.tf
@@ -1,6 +1,5 @@
 module "backend" {
   source              = "git::https://github.com/OrganizacionDevOps/OptimizApp_Infraestrucutra_Modular.git//modules/backend/aws?ref=main"
-"
   aws_account_id      = var.aws_account_id
   oidc_role_name      = var.oidc_role_name
   bucket_name         = var.bucket_name
