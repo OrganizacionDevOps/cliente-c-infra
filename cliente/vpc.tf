a14cea2cc8e8bd4fa0ebf434c86f5d5e3fe6e66e
servicesprueba <itoperations@optimizapp.com>
2025-07-07 16:49:05 -0500

    Update vpc.tf

--- cliente/vpc.tf
+++ cliente/vpc.tf
@@ -1,13 +1,10 @@
 module "vpc" {
-  source = "git::https://github.com/OrganizacionDevOps/OptimizApp_Infraestrucutra_Modular.git//modules/networking/vpc?ref=main"
-
-  client_name  = var.client_name
-  environment  = var.environment
-  region       = var.region
-
-  cidr_block   = var.vpc_cidr_block
-  enable_nat   = var.enable_nat_gateway
-  enable_dns   = var.enable_dns_support
+  source        = "git::https://github.com/OrganizacionDevOps/OptimizApp_Infraestrucutra_Modular.git//modules/networking/vpc?ref=main"
+  
+  vpc_name      = "${var.client_name}-${var.environment}-vpc"
+  cidr_block    = var.vpc_cidr_block
+  environment   = var.environment
+  kms_key_id    = var.kms_key_id
 
   tags = {
     Client      = var.client_name
