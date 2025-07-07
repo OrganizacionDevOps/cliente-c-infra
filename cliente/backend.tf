c0ea834de5ae2680ec46f3882e93f9ccdcd2032d
servicesprueba <itoperations@optimizapp.com>
2025-07-07 16:28:54 -0500

    Update backend.tf

--- cliente/backend.tf
+++ cliente/backend.tf
@@ -1,11 +1,10 @@
 module "backend" {
   source              = "git::https://github.com/OrganizacionDevOps/OptimizApp_Infraestrucutra_Modular.git//modules/backend/aws?ref=main"
+
+  client_name         = var.client_name
+  environment         = var.environment
   aws_account_id      = var.aws_account_id
   oidc_role_name      = var.oidc_role_name
   bucket_name         = var.bucket_name
   dynamodb_table_name = var.dynamodb_table_name
-  tags = {
-    Client      = var.client_name
-    Environment = var.environment
-  }
 }
