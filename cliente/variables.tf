f3c9baf9413c99d79cb58ee31cb6775e81596be9
servicesprueba <itoperations@optimizapp.com>
2025-07-07 16:48:47 -0500

    Update variables.tf

--- cliente/variables.tf
+++ cliente/variables.tf
@@ -6,16 +6,4 @@ variable "bucket_name" {}
 variable "dynamodb_table_name" {}
 variable "region" {}
 
-module "vpc" {
-  source        = "git::https://github.com/OrganizacionDevOps/OptimizApp_Infraestrucutra_Modular.git//modules/networking/vpc?ref=main"
-  
-  vpc_name      = "${var.client_name}-${var.environment}-vpc"
-  cidr_block    = var.vpc_cidr_block
-  environment   = var.environment
-  kms_key_id    = var.kms_key_id
 
-  tags = {
-    Client      = var.client_name
-    Environment = var.environment
-  }
-}
