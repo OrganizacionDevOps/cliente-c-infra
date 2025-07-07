293cb2a6fdd34dbf867de9cbdf8d3f2dcdaf5587
servicesprueba <itoperations@optimizapp.com>
2025-07-07 16:40:04 -0500

    Update variables.tf

--- cliente/variables.tf
+++ cliente/variables.tf
@@ -5,3 +5,20 @@ variable "oidc_role_name" {}
 variable "bucket_name" {}
 variable "dynamodb_table_name" {}
 variable "region" {}
+
+variable "vpc_cidr_block" {
+  description = "Bloque CIDR para la VPC"
+  type        = string
+}
+
+variable "enable_nat_gateway" {
+  description = "Habilita NAT Gateway (true/false)"
+  type        = bool
+  default     = false
+}
+
+variable "enable_dns_support" {
+  description = "Habilita soporte DNS"
+  type        = bool
+  default     = true
+}
