ab04ff97070553c070f9b97df435cc4392770d42
CristianDussan <crissdussan@hotmail.es>
2025-07-09 13:16:45 -0500

    cambio script cliente a privado

--- cliente/vpc.tf
+++ cliente/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source        = "git::ssh://git@github.com/OrganizacionDevOps/OptimizApp_Infraestrucutra_Modular.git//modules/vpc?ref=main"
+  source        = "git::ssh://git@github.com/OrganizacionDevOps/OptimizApp_Infraestrucutra_Modular.git//modules/networking/vpc?ref=main"
   
   vpc_name      = "${var.client_name}-${var.environment}-vpc"
   cidr_block    = var.vpc_cidr_block
