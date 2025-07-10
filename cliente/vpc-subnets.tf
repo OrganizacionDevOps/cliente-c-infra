669b305893085a33ca5aa8662c30e8f57d7206aa
CristianDussan <crissdussan@hotmail.es>
2025-07-10 11:26:30 -0500

    prueba modulo subnet

---
+++ cliente/vpc-subnets.tf
@@ -0,0 +1,9 @@
+module "subnets" {
+  source  = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_subnets_module.git?ref=v1.0.0"
+
+  vpc_id              = var.vpc_id
+  public_cidr_block   = var.public_cidr_block
+  private_cidr_block  = var.private_cidr_block
+  availability_zone   = var.availability_zone
+  environment         = var.environment
+}
