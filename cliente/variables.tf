66d13a6b9ca53900f044d6c174a235450f1fa44f
CristianDussan <crissdussan@hotmail.es>
2025-07-10 12:53:34 -0500

    prueba integracion modulos

--- cliente/variables.tf
+++ cliente/variables.tf
@@ -5,12 +5,11 @@ variable "oidc_role_name" {}
 variable "bucket_name" {}
 variable "dynamodb_table_name" {}
 variable "region" {}
-variable "vpc_cidr_block" {}
+
 variable "kms_key_id" {}
 variable "vpc_id" {}
-variable "cidr_blocks" {
-  type = list(string)
-}
+
+
 variable "availability_zones" {
   type = list(string)
 }
