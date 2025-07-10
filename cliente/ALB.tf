5e2fcaffe69824a3eb8c278dab712cbe0df73386
CristianDussan <crissdussan@hotmail.es>
2025-07-10 16:29:07 -0500

    prueba integracion modulos

--- cliente/ALB.tf
+++ cliente/ALB.tf
@@ -1,7 +1,7 @@
-module "sg_alb" {
-  source        = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_sg-alb_module.git?ref=main"
-  vpc_id        = module.vpc.vpc_id
-  environment   = var.environment
-  alb_ports     = [80, 443]
-  alb_allowed_cidr_blocks = ["0.0.0.0/0"]
+module "alb" {
+  source            = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_alb_module.git?ref=main"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.subnets.public_subnet_ids
+  alb_sg_id         = module.sg_alb.alb_sg_id
+  environment       = var.environment
 }
