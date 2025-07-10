fe4f351e9188522ebe30798bd8c1b9d0576fa0b1
CristianDussan <crissdussan@hotmail.es>
2025-07-10 16:30:22 -0500

    prueba integracion modulos

--- cliente/ALB.tf
+++ cliente/ALB.tf
@@ -1,5 +1,6 @@
 module "alb" {
-  source            = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_alb_module.git?ref=main"
+  source            = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_AWS_ALB_module
+.git?ref=main"
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.subnets.public_subnet_ids
   alb_sg_id         = module.sg_alb.alb_sg_id
