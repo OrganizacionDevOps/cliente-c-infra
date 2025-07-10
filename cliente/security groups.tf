2a9e35062b4597dcff0bb7f584b4c81ee776e992
CristianDussan <crissdussan@hotmail.es>
2025-07-10 12:03:19 -0500

    prueba modulo subnet

---
+++ cliente/security groups.tf
@@ -0,0 +1,34 @@
+module "sg" {
+  source = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_security_groups_module.git?ref=main"
+
+  vpc_id      = var.vpc_id
+  sg_name     = "sg-app-${var.environment}"
+  environment = var.environment
+
+  ingress_rules = [
+    {
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "Allow HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress_rules = [
+    {
+      description = "Allow all outbound traffic"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
