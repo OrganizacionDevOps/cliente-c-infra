e207b766298cf0eef77a767b397ead430bf64488
CristianDussan <crissdussan@hotmail.es>
2025-07-11 14:33:30 -0500

    prueba integracion modulos

--- cliente/igw.tf
+++ cliente/igw.tf
@@ -1,5 +1,5 @@
 module "internet_gateway" {
-  source      = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_Internet_Gateway_module.git?ref=main"
+  source      = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_internet_Gateway_module.git?ref=main"
   
   vpc_id      = module.vpc.vpc_id
   client_name = var.client_name        # aquí pasas la variable que sí tienes
