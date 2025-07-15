6f3b7edf42a2cd5fda8aaddca2da1e348bf6730e
CristianDussan <crissdussan@hotmail.es>
2025-07-15 13:01:18 -0500

    prueba integracion modulos

--- cliente/IAM_Policies.tf
+++ cliente/IAM_Policies.tf
@@ -1,9 +1,16 @@
 module "iam_roles" {
-  source              = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_IAM_Roles_module.git?ref=main"
-  client_name         = var.client_name
-  bucket_name         = var.bucket_name
-  kms_key_arn         = var.kms_key_arn
-  region              = var.region
-  account_id          = var.aws_account_id
-  dynamodb_table_name = var.dynamodb_table_name
+  source = "git::ssh://git@github.com/OrganizacionDevOps/Optimizapp_IAM_Roles_module.git?ref=main"
+
+  client_name          = var.client_name
+  environment          = var.environment
+  region               = var.region
+  account_id           = var.aws_account_id
+  bucket_name          = var.bucket_name
+  dynamodb_table_name  = var.dynamodb_table_name
+  kms_key_arn          = var.kms_key_arn
+  project              = "cliente-c-infra"
+
+  # Nuevas variables:
+  oidc_provider_arn = "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
+  oidc_provider_url = "token.actions.githubusercontent.com"
 }
