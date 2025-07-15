0021cf7a1229f28ed27c2ef3aa19d583aad11648
CristianDussan <crissdussan@hotmail.es>
2025-07-15 11:15:42 -0500

    prueba integracion modulos

---
+++ cliente/IAM_Policies.tf
@@ -0,0 +1,9 @@
+module "iam_roles" {
+  source              = "git::ssh://git@github.com/ORG/Optimizapp_IAM_Roles_module.git?ref=main"
+  client_name         = var.client_name
+  bucket_name         = var.bucket_name
+  kms_key_arn         = var.kms_key_arn
+  region              = var.region
+  account_id          = var.account_id
+  dynamodb_table_name = var.dynamodb_table_name
+}
